ba9bc8092224e319f2f8cdcb0d642db53e813692
josssss07 <joshuadesai03@gmail.com>
2024-01-11 09:12:26 +0530

    added button to open site

--- prac4/lib/pages/homepage.dart
+++ prac4/lib/pages/homepage.dart
@@ -1,9 +1,25 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_interpolation_to_compose_strings
 
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  void launchBrowser() async {
+    const url = 'https://xaviers.ac/';
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      print('Kill yourself UwU');
+    }
+  }
+
+  void showSnackBar(BuildContext context, String text) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Pressed $text')));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +30,24 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
+            TextButton(
+              onPressed: () {
+                showSnackBar(context, "Text Button");
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.disabled_by_default),
+                  Text('Disabled')
+                ],
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  launchBrowser();
+                },
+                child: Text("open xaviers site"))
           ],
         ),
       ),
