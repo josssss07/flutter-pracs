f060537ea42f0b530d0063876964a1fef6b8f3ee
josssss07 <joshuadesai03@gmail.com>
2024-01-11 08:39:46 +0530

    structure added again

---
+++ prac4/lib/pages/homepage.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Button but BIGGER'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
