7302abbc02ae8388925e36ce3ca478057a981437
josssss07 <joshuadesai03@gmail.com>
2024-01-11 10:40:47 +0530

    pract 5 init

---
+++ prac5/lib/pages/homepage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stuff Calcuator'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+              ElevatedButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => RectanglePage()),
+                );
+              }, child: Text("area of Rectangle")),
+              ElevatedButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => EMIPage()),
+                );
+              }, child: Text("area of Rectangle")),
+              ElevatedButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => BMIPage()),
+                );
+              }, child: Text("area of Rectangle")),
+          ],
+        ),
+      ),
+    );
+  }
+}
