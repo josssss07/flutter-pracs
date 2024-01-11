d8ba881c323facd9d788c12f6043e333d5232ec1
NikthePhoenix <nikgov23@gmail.com>
2024-01-11 09:32:54 +0530

    Modified prac 4

--- prac4/lib/pages/homepage.dart
+++ prac4/lib/pages/homepage.dart
@@ -1,24 +1,23 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_interpolation_to_compose_strings
 
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
-  void launchBrowser() async {
+  void launchBrowser(BuildContext context) async {
     const url = 'https://xaviers.ac/';
     if (await canLaunchUrlString(url)) {
       await launchUrlString(url);
     } else {
-      print('Kill yourself UwU');
+      // print('failure');
+      showSnackBar(context, 'Failure');
     }
   }
 
   void showSnackBar(BuildContext context, String text) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text('Pressed $text')));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
   }
 
   @override
@@ -31,12 +30,15 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             TextButton(
               onPressed: () {
-                showSnackBar(context, "Text Button");
+                showSnackBar(context, "Pressed Text Button");
               },
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Icon(Icons.disabled_by_default),
                   Text('Disabled')
@@ -44,10 +46,18 @@ class HomePage extends StatelessWidget {
               ),
             ),
             ElevatedButton(
-                onPressed: () {
-                  launchBrowser();
-                },
-                child: Text("open xaviers site"))
+              onPressed: () {
+                launchBrowser(context);
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.cast_for_education),
+                  Text('Xaviers.ac site (why?)')
+                ],
+              ),
+            )
           ],
         ),
       ),
