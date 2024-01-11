4698a12e90f1c0590828d0fafb21fbfd16a57552
josssss07 <joshuadesai03@gmail.com>
2024-01-11 10:21:31 +0530

    formpage added

---
+++ prac4/lib/pages/formpage.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Formpage extends StatelessWidget {
+  const Formpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: key, 
+      child: Column(
+        children: <Widget>[
+          
+        ],
+      ),);
+  }
+}
