e7a3f52936a75d885f0bc1ac697861a772b4f28a
josssss07 <joshuadesai03@gmail.com>
2024-01-11 10:29:17 +0530

    del files

--- prac4/lib/pages/calc.dart
+++
@@ -1,9 +0,0 @@
-class BMICalculator {
-  int weight = 0;
-  int height = 0;
-//BMI is weight in kilograms divided by height in meters squared.
-
-  double calculate(int weight, int height) {
-    return (weight / height) * (weight / height);
-  }
-}
