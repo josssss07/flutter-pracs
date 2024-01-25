9654b38ff7f4130034245e148e218d3a2c92b967
josssss07 <joshuadesai03@gmail.com>
2024-01-25 09:29:26 +0530

    prac7 init

---
+++ prac7_crud/README.md
@@ -0,0 +1,3 @@
+# prac7_crud
+
+A new Flutter project.
---
+++ prac7_crud/android/app/src/main/kotlin/com/example/prac7_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prac7_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
